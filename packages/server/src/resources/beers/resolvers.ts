1ca0ce3cf7b0f7673e5545a6a5fd937152b941b8
Vitor Freitas <vitorldfreitas@gmail.com>
2020-01-10 19:22:16 -0300

    refactor: remove try/catch in order to use error handling middleware

--- packages/server/src/resources/beers/resolvers.ts
+++ packages/server/src/resources/beers/resolvers.ts
@@ -4,63 +4,31 @@ import Beer from './model'
 export const createBeer = async ctx => {
   const beer: IBeer = ctx.request.body
 
-  try {
-    await Beer.create(beer)
+  await Beer.create(beer)
 
-    ctx.body = {
-      message: 'Beer successfully registered'
-    }
-  } catch (err) {
-    ctx.status = err.status || 500
-    ctx.body = {
-      error: err.message,
-      message: 'Error creating a new beer. Try again later'
-    }
+  ctx.body = {
+    message: 'Beer successfully registered'
   }
 }
 
 export const findBeers = async ctx => {
-  try {
-    const beers = await Beer.find()
+  const beers = await Beer.find()
 
-    ctx.body = { beers }
-  } catch (err) {
-    ctx.status = err.status || 500
-    ctx.body = {
-      error: err.message,
-      message: 'Error finding beers. Try again later'
-    }
-  }
+  ctx.body = { beers }
 }
 
 export const updateBeer = async ctx => {
-  try {
-    await Beer.findByIdAndUpdate(ctx.params.id, ctx.request.body)
+  await Beer.findByIdAndUpdate(ctx.params.id, ctx.request.body)
 
-    ctx.body = {
-      message: 'Beer successfully updated'
-    }
-  } catch (err) {
-    ctx.status = err.status || 500
-    ctx.body = {
-      error: err.message,
-      message: 'Error updating this beer. Try again later'
-    }
+  ctx.body = {
+    message: 'Beer successfully updated'
   }
 }
 
 export const removeBeer = async ctx => {
-  try {
-    await Beer.findByIdAndRemove(ctx.params.id)
+  await Beer.findByIdAndRemove(ctx.params.id)
 
-    ctx.body = {
-      message: 'Beer successfully removed'
-    }
-  } catch (err) {
-    ctx.status = err.status || 500
-    ctx.body = {
-      error: err.message,
-      message: 'Error removing this beer. Try again later'
-    }
+  ctx.body = {
+    message: 'Beer successfully removed'
   }
 }
